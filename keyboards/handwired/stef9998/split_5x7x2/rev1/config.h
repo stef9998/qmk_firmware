99fcf58cfd65d25f74f9fe9463cfc93c33b14e32
stef9998 <stef9998@mail.de>
2022-08-21 16:35:04 +0200

    remove RGB

--- keyboards/handwired/stef9998/split_5x7x2/rev1/config.h
+++ keyboards/handwired/stef9998/split_5x7x2/rev1/config.h
@@ -41,34 +41,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
-/* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
-
-/* number of backlight levels */
-// #define BACKLIGHT_LEVELS 3
-
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 
 /* serial.c configuration for split keyboard */
 #define SOFT_SERIAL_PIN D0
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-/* ws2812 RGB LED*/
-#define RGB_DI_PIN D3
-
-#define RGBLED_NUM 14    // Number of LEDs
-
-#define RGBLIGHT_SPLIT   // sync LEDs between RIGHT and LEFT hand
 
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
  */
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
 
 /* disable debug print */
 // #define NO_DEBUG
